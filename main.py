1a87979176d7cb6449a54b2f734c30ffb3fb6af8
stud304 <golikova_v06@mail.ru>
2023-03-22 11:27:28 +0300

    Стас опять помог

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
 
-host = 'http://127.0.0.1:5000/sum'
-r = requests.post(host,json={'a': 4, 'b': 9})
+host = 'http://128.1.12.88:5000/sum3/'
+'''
+r = requests.post(host,json=  {'a':1,'b':2})
+assert r.json() == {'sum':3}
+'''
+'''
+r = requests.post(host,json= {'data':[ 1, 2, 3 ]})
 
-assert r.json() == {'sum': 13}
+assert r.json() == {'sum':6}
+'''
+r = requests.post(host,json= {'a':1,'b':2, 'c':3})
+assert r.json() == {'sum':6}
 
-r = requests.post(host,json={'a': -4, 'b': -9})
 
-assert r.json() == {'sum': -13}
+
+
 
 
--- xxxxxx.py
+++ xxxxxx.py
@@ -3,12 +3,27 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/sum', methods=['POST'])
+@app.route('/sum3/', methods=['POST'])
 def hello_world():
+    '''
     print(request.json)
+
     a = request.json['a']
     b = request.json['b']
-    return {'sum': a + b}
+    return {'sum': a+b}
+    '''
+    '''
+    data = request.json['data']
+    print({'sum':data})
+    return {'sum':sum(data)}
+    '''
+    print(request.json)
+
+    a = request.json['a']
+    b = request.json['b']
+    c = request.json['c']
+    return {'sum': a + b + c}
 
 
-app.run()
+
+app.run(host='128.1.12.88', port=5000)
